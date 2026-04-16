a2c6d5e646a655ca14e116f91e3c87efb9927097
ans-mishra <ans.rajan.mishra@gmail.com>
2026-04-16 22:35:44 +0530

    Set output to 'export' in next.config.js
    Added output property to nextConfig for export mode.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
+// const path = require("path");
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   outputFileTracingRoot: path.join(__dirname),
+//   webpack: (config) => {
+//     config.externals.push("pino-pretty", "lokijs", "encoding");
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // 👈 ADD THIS
+
   outputFileTracingRoot: path.join(__dirname),
+
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
